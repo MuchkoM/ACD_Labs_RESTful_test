dade9ee664e5b94d593b03db3a438b6b32ff65b5
Muchkom <muchkom@gmail.com>
2018-10-17 15:54:19 +0300

    add layer abstraction Task add Table class add Custom exception add docstring rename json param

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import Iterable
+from copy import deepcopy
 from itertools import groupby
 
 import numpy as np
@@ -5,48 +7,214 @@ from flask import Flask, request
 from flask_restful import Api, Resource
 
 
-def is_valid(table):
+class TableStringSortingTask:
     """
-    Table valid check
-    :return: True is valid, False is not valid
+        TableStringSorting performer
     """
-    if table is None:
-        return False
 
-    g = groupby(table, key=len)
-    return next(g, True) and not next(g, False)
+    def __init__(self, table_string, separator_column, separator_row):
+        """
+        Init task param
+        :param table_string: String with table
+        :param separator_column:  Column separator
+        :param separator_row: Row separator
+        """
+        self.input_sting = table_string
+        self.converter = Converter(ArrayTable, separator_column=separator_column, separator_row=separator_row)
+        self.sorter = Sorter()
+        self.output_string = None
+        self.isSuccess = False
+
+    def perform(self):
+        """
+        Task performer
+        """
+        table = self.converter.to_table(self.input_sting)
+        table_sorted = self.sorter.sort(table)
+        self.output_string = self.converter.to_string(table_sorted)
+        self.isSuccess = True
+
+    def result(self):
+        """
+        Result of the task
+        :return: String with sorted table
+        """
+        return self.output_string
+
+
+class TableStringException(Exception):
+    """
+    Exception for TableStringSortingTask
+    """
+
+    def __init__(self, message=None, code=None, *args, **kwargs):
+        """
+        Init TableStringException
+        :param message: Error description
+        :param code: HTTP code of error
+        :param args: args for super
+        :param kwargs: kwargs for super
+        """
+        super().__init__(args, kwargs)
+        self.message = message or "TableString exception"
+        self.code = code or 422
 
 
 class Converter:
-    """Class converter string to 2D array and back"""
+    """
+    Class converter `str` to `class.Table` and back
+    """
 
-    def __init__(self, separator_column=None, separator_row=None):
-        """ Set separator for string """
+    def __init__(self, cls, separator_column=None, separator_row=None):
+        """
+        Init class,separator setting for converter
+        :param cls: Target class to convert
+        :param separator_column:  Column separator
+        :param separator_row: Row separator
+        """
+        if separator_column == '':
+            raise TableStringException("Column separator is empty")
+        if separator_row == '':
+            raise TableStringException("Row separator is empty")
+
         self.separator_column = separator_column or '\n'
         self.separator_row = separator_row or '\t'
+        self.cls = cls
 
-    def to_table(self, string):
-        """ Convert string to 2D array with """
+    def to_table(self, table_string):
+        """
+        Convert string to NumPy 2D-array
+        :param table_string: String to parse
+        :return: Valid NumPy 2D-array
+        :except Rise TableStringException with error message and status code
+        """
+        if table_string is None:
+            raise TableStringException("Table_string is undefined")
+        if table_string == "":
+            raise TableStringException("Table_string is empty")
+
         array = []
-        for str_col in string.split(self.separator_column):
+        for str_col in table_string.split(self.separator_column):
             str_cells = str_col.split(self.separator_row)
             array.append(str_cells)
 
-        if not is_valid(array):
-            raise ValueError()
+        table = self.cls(array)
 
-        return np.array(array)
+        if not table.is_valid():
+            raise TableStringException("Table is not valid")
+
+        return table
 
     def to_string(self, table):
+        """
+        Convert string to NumPy 2D-array with
+        :param table: 2D-array to convert
+        :return: String interpretation of array
+        :except Rise TableStringException with error message and status code
+        """
         str_list = []
-        for col in table:
+        for col in table.to_array():
             str_list.append(self.separator_row.join(col))
         return self.separator_column.join(str_list)
 
 
+class Table:
+    """
+    Abstract array
+    """
+    table = None
+
+    def copy(self):
+        """
+        Copy of object
+        :return: deepcopy of self
+        """
+        return deepcopy(self)
+
+    def to_array(self):
+        """
+        Transform object to array
+        :return: Array representation
+        """
+        pass
+
+    def is_valid(self):
+        """
+        Table valid check
+        :return: True is valid, False is not valid
+        """
+        if self.table is None:
+            return False
+        if not isinstance(self.table, Iterable):
+            return False
+
+        g = groupby(self.table, key=len)
+        return next(g, True) and not next(g, False)
+
+    def sort(self, axis, key):
+        """
+        Sort self.table
+        :param axis: sort axis
+        :param key: key function for sort
+        """
+        pass
+
+    def __str__(self):
+        """
+        String representation of self.table
+        :return: representation
+        """
+        return str(self.table)
+
+
+# class NumPyTable(Table):
+#     def __init__(self, array):
+#         self.table: np.ndarray = np.array(array)
+#
+#     def to_array(self):
+#         return self.table.tolist()
+#
+#     def sort(self, axis, key):
+#         """
+#         Sort self.table
+#         :param axis: sort axis
+#         :param key: key function for sort
+#         """
+#         self.table = np.apply_along_axis(func1d=sorted, axis=axis, arr=self.table, key=key)
+
+
+class ArrayTable(Table):
+    def __init__(self, array):
+        self.table = array
+
+    def to_array(self):
+        return deepcopy(self.table)
+
+    def transpose(self):
+        """
+        Transpose self.table
+        :return:
+        """
+        self.table = [list(x) for x in zip(*self.table)]
+
+    def sort(self, axis, key):
+        if axis == 0:
+            self.transpose()
+        for col in self.table:
+            col.sort(key=key)
+        if axis == 0:
+            self.transpose()
+
+
 class Sorter:
-    def sort(self, table):
-        def key(x):
+    def __init__(self, axis=0, key=None):
+        """
+        Init Sorter
+        :param axis: axis of sort : 0 is vertical, 1 is horizontal
+        :param key: key function for sorting
+        """
+
+        def default_key_sort(x):
             """Default sorter: str > number"""
             try:
                 val = float(x)
@@ -54,7 +222,17 @@ class Sorter:
             except ValueError:
                 return 1, x
 
-        return np.apply_along_axis(func1d=sorted, axis=0, arr=table, key=key)
+        self.key = key or default_key_sort
+        self.axis = axis
+
+    def sort(self, table):
+        """
+        Sorting table with self.key function
+        :return: `class.Table` sorted
+        """
+        sorted_table = table.copy()
+        sorted_table.sort(axis=self.axis, key=self.key)
+        return sorted_table
 
 
 app = Flask(__name__)
@@ -63,38 +241,31 @@ api = Api(app)
 
 class TableStringResource(Resource):
     def put(self):
-        """ Sorting table in string format from json table"""
-        json_data = request.get_json(force=True)
+        """ Sorting table in string format from json string_table"""
+        json_data = request.get_json()
 
-        table_string = json_data.get('table', None)
-        separator_row = json_data.get('row_det', None)
+        table_string = json_data.get('table_string', None)
+        separator_row = json_data.get('separator_row', None)
         separator_column = json_data.get('separator_column', None)
 
-        if table_string is None or table_string == "":
-            json_data['message'] = 'String is empty or undefined'
-            return json_data, 400
-
-        converter = Converter(separator_column=separator_column, separator_row=separator_row)
-
         try:
-            table = converter.to_table(table_string)
-        except ValueError:
-            json_data['message'] = 'Table is not valid'
-            return json_data, 422
+            task = TableStringSortingTask(table_string=table_string, separator_column=separator_column,
+                                          separator_row=separator_row)
+            task.perform()
+            if task.isSuccess:
+                sorted_table_string = task.result()
+            else:
+                raise TableStringException
 
-        sorter = Sorter()
+        except TableStringException as e:
+            json_data['message'] = e.message
+            return json_data, e.code
 
-        sorted_table = sorter.sort(table)
-
-        sorted_string_table = converter.to_string(sorted_table)
-
-        json_data['table'] = sorted_string_table
+        json_data['table_string'] = sorted_table_string
         return json_data, 200
 
 
 api.add_resource(TableStringResource, '/table_string')
 if __name__ == '__main__':
-    """
-    Start point
-    """
+    """Start point"""
     app.run(debug=True)
--- test.py
+++ test.py
@@ -21,68 +21,49 @@ class TestConverter(unittest.TestCase):
 class TestSorter(unittest.TestCase):
 
     def test_data_sort1(self):
-        data = [['11', '22', '31'], ['44', '15', '63'], ['57', '88', '19'], ['88', '99', '99']]
-        sorted_data = [['11', '15', '19'], ['44', '22', '31'], ['57', '88', '63'], ['88', '99', '99']]
+        data = np.array([['11', '22', '31'], ['44', '15', '63'], ['57', '88', '19'], ['88', '99', '99']])
+        sorted_data = np.array([['11', '15', '19'], ['44', '22', '31'], ['57', '88', '63'], ['88', '99', '99']])
 
         test_data = Sorter().sort(data)
-        return self.assertEqual(sorted_data, test_data)
+        return self.assertTrue(np.equal(sorted_data, test_data))
 
     def test_data_sort2(self):
-        data = [['a', 'b', 'c'], ['11', '22', '31'], ['44', '15', '63'], ['-', '88', '19'], ['88', '99', '99']]
-        sorted_data = [['11', '15', '19'], ['44', '22', '31'], ['88', '88', '63'], ['-', '99', '99'], ['a', 'b', 'c']]
+        data = np.array(
+            [['a', 'b', 'c'], ['11', '22', '31'], ['44', '15', '63'], ['-', '88', '19'], ['88', '99', '99']])
+        sorted_data = np.array(
+            [['11', '15', '19'], ['44', '22', '31'], ['88', '88', '63'], ['-', '99', '99'], ['a', 'b', 'c']])
 
         test_data = Sorter().sort(data)
-        return self.assertEqual(sorted_data, test_data)
+        return self.assertTrue(np.equal(sorted_data, test_data))
 
 
 class TestTableStringSorter(unittest.TestCase):
 
-    def test_table_str_sort(self):
-        string = "1\t2a\t3\n4\t53\t6\n7\t8\t9"
-        sorted_string = "1\t8\t3\n4\t53\t6\n7\t2a\t9"
+    def test_table_str_sort_string(self):
+        string = "a\t2a\t3\nab\t53\t9\nab-\t8\t6"
+        sorted_string = "a\t8\t3\nab\t53\t6\nab-\t2a\t9"
 
         converter = Converter()
-        data = converter.to_table(string)
         sorter = Sorter()
-        sorted_data = sorter.sort(data)
 
+        data = converter.to_table(string)
+        sorted_data = sorter.sort(data)
         sorted_string_test = converter.to_string(sorted_data)
 
         return self.assertEqual(sorted_string, sorted_string_test)
 
+    def test_table_str_sort_string_2(self):
+        string = "1\t2a\t3\n4\t53\t6\n7\t8\t9"
+        sorted_string = "a\t8\t3\nab\t53\t6\nab-\t2a\t9"
 
-class TestSorterNumPy(unittest.TestCase):
+        converter = Converter()
+        sorter = Sorter()
 
-    def test_data_sort1(self):
-        data = [['11', '22', '31'], ['44', '15', '63'], ['57', '88', '19'], ['88', '99', '99']]
-        sorted_data = [['11', '15', '19'], ['44', '22', '31'], ['57', '88', '63'], ['88', '99', '99']]
-        np_data = np.array(data)
-        np_result_data = np.array(sorted_data)
-        np_data.sort(axis=0)
-        cmp = np.array_equal(np_data, np_result_data)
+        table = converter.to_table(string)
+        sorted_data = sorter.sort(table)
+        sorted_string_test = converter.to_string(sorted_data)
 
-        return self.assertTrue(cmp)
-
-    def test_data_sort2(self):
-        data = [['a', 'b', 'c'], ['11', '22', '31'], ['44', '15', '63'], ['-', '88', '19'], ['88', '99', '99']]
-
-        np_data = np.array(data)
-
-        def key_func_default(x):
-            """Default sorter: str > number"""
-            try:
-                val = float(x)
-                return -1, val
-            except ValueError:
-                return 1, x
-
-        np_data = np.apply_along_axis(func1d=sorted, axis=0, arr=np_data, key=key_func_default)
-
-        sorted_data = [['11', '15', '19'], ['44', '22', '31'], ['88', '88', '63'], ['-', '99', '99'], ['a', 'b', 'c']]
-        np_result_data = np.array(sorted_data)
-        cmp = np.array_equal(np_data, np_result_data)
-
-        return self.assertTrue(cmp)
+        return self.assertEqual(sorted_string_test, sorted_string, )
 
 
 if __name__ == '__main__':
